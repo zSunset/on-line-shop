ec205a43d79b5f1202d6c50f924a6e4cad6c464d
zSunset <85899844+zSunset@users.noreply.github.com>
2023-05-20 21:01:07 +0300

    chapter_3 (#3)

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart:
@@ -14,6 +15,8 @@ class Cart:
             # сохранить пустую карзину
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # сохраняет текущий примененный купон
+        self.coupon_id = self.session.get('coupon_id')
 
     def __iter__(self):
         """
@@ -71,3 +74,20 @@ class Cart:
         # удалить корзину из сеанса
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100) * self.get_total_price())
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
