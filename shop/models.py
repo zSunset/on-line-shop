00c285ee1f55eb5a2f9c2c2af5b9cf2be9eac9fc
zSunset <85899844+zSunset@users.noreply.github.com>
2023-05-21 19:19:20 +0300

    Chapter 4 (#4)
    * false
    
    * false
    
    * chapter_4

--- shop/models.py
+++ shop/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from django.urls import reverse
+from parler.models import TranslatableModel, TranslatedFields
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250,
-                            unique=True)
+class Category(TranslatableModel):
+    translations = TranslatedFields(
+        name=models.CharField(max_length=250),
+        slug=models.SlugField(max_length=250,
+                              unique=True))
 
     class Meta:
-        ordering = ['name']
-        indexes = [
-            models.Index(fields=['name']),
-        ]
+        # ordering = ['name']
+        # indexes = [
+        #    models.Index(fields=['name']),]
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
@@ -22,14 +23,15 @@ class Category(models.Model):
         return reverse('shop:product_list_by_category', args=[self.slug])
 
 
-class Product(models.Model):
+class Product(TranslatableModel):
+    translations = TranslatedFields(name=models.CharField(max_length=200),
+                                    slug=models.SlugField(max_length=200), description=models.TextField(blank=True))
     category = models.ForeignKey(
         Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
+
     image = models.ImageField(upload_to='products/%Y/%m/%d',
                               blank=True)
-    description = models.TextField(blank=True)
+
     price = models.DecimalField(max_digits=10,
                                 decimal_places=2)
     available = models.BooleanField(default=True)
@@ -37,10 +39,10 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['name']
+        # ordering = ['name']
         indexes = [
-            models.Index(fields=['slug', 'id']),
-            models.Index(fields=['name']),
+            # models.Index(fields=['slug', 'id']),
+            # models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
 
